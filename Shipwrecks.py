d9b0f3ae83cfa03d120b5c7a6b741b752d7b44a1
izaromanowska <izaromanowska@gmail.com>
2018-02-14 16:09:49 +0100

    Quick data exploration of roman shipwrecks
    First upload

---
+++ Shipwrecks.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 14:31:12 2018
+
+@author: iromanow
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+df = pd.read_excel('StraussShipwrecks.xlsx') 
+
+
+
